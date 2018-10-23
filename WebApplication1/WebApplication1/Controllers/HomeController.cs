ff9aa355044de5009fcc68b2e45ad0b0254fde1a
Ahmad Maksousa <ah_maksousa@hotmail.com>
2018-10-23 12:29:57 +0300

    initCommit

--- WebApplication1/WebApplication1/Controllers/HomeController.cs
+++ WebApplication1/WebApplication1/Controllers/HomeController.cs
@@ -12,6 +12,7 @@ namespace WebApplication1.Controllers
     {
         public IActionResult Index()
         {
+            //123
             return View();
         }
 
--- WebApplication1/WebApplication1/Controllers/tajController.cs
+++ WebApplication1/WebApplication1/Controllers/tajController.cs
@@ -11,6 +11,7 @@ namespace WebApplication1.Controllers
     [ApiController]
     public class tajController : ControllerBase
     {
+        //
     }
 
 }
