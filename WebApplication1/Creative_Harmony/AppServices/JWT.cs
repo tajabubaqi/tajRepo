19a64f646416efbf4236d0dddea3bd5f55cc8951
Ahmad Maksousa <ah_maksousa@hotmail.com>
2018-11-01 17:09:32 +0200

    JWT Class

---
+++ WebApplication1/Creative_Harmony/AppServices/JWT.cs
@@ -0,0 +1,29 @@
+﻿using Creative_Harmony.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Creative_Harmony.AppServices
+{
+    public class JWT
+    {
+        public static string BuildToken(IConfiguration _config)
+        {
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config["Jwt:Key"]));
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+            var token = new JwtSecurityToken(_config["Jwt:Issuer"],
+              _config["Jwt:Issuer"],
+              expires: DateTime.Now.AddMinutes(30),
+              signingCredentials: creds);
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+    }
+}
--- WebApplication1/Creative_Harmony/Controllers/AdminController.cs
+++ WebApplication1/Creative_Harmony/Controllers/AdminController.cs
@@ -4,6 +4,7 @@ using System.IdentityModel.Tokens.Jwt;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Creative_Harmony.AppServices;
 using Creative_Harmony.Models;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
@@ -27,6 +28,23 @@ namespace Creative_Harmony.Controllers
             return View();
         }
 
+        [HttpPost]
+        public IActionResult CreateToken(IFormCollection user)
+        {
+            IActionResult response = Unauthorized();
+            var context = new HarmonyContext();
+
+            bool isAuthorized = context.users.Any(u => user["email"] == u.Name && user["pass"] == u.Password);
+
+            if (isAuthorized)
+            {
+                var tokenString = JWT.BuildToken(_config);
+                response = Ok(new { token = tokenString });
+            }
+
+            return response;
+        }
+
         [HttpGet, Authorize]
         public IEnumerable<Users> GetAll()
         {
@@ -35,43 +53,5 @@ namespace Creative_Harmony.Controllers
             return users;
         }
 
-        [AllowAnonymous]
-        [HttpPost]
-        public IActionResult CreateToken(IFormCollection Logginguser)
-        {
-            IActionResult response = Unauthorized();
-            var user = Authenticate(Logginguser);
-
-            if (user != null)
-            {
-                var tokenString = BuildToken(user);
-                response = Ok(new { token = tokenString });
-            }
-
-            return response;
-        }
-
-        private string BuildToken(Users user)
-        {
-            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config["Jwt:Key"]));
-            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
-
-            var token = new JwtSecurityToken(_config["Jwt:Issuer"],
-              _config["Jwt:Issuer"],
-              expires: DateTime.Now.AddMinutes(30),
-              signingCredentials: creds);
-
-            return new JwtSecurityTokenHandler().WriteToken(token);
-        }
-
-        private Users Authenticate(IFormCollection user)
-        {
-            var context = new HarmonyContext();
-            List<Users> users = context.users.Where(u => user["email"] == u.Name && user["pass"] == u.Password).ToList<Users>();
-            if (users.Count > 0)
-                return users[0];
-            else return null;
-
-        }
     }
 }
