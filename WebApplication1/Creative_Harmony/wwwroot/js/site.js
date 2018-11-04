97d93b464f7320019f30dcdc0382e03830ca2a84
tabubaqi <tabubaqi1995@hotmail.com>
2018-11-04 20:15:17 +0200

    Add Animation To Views

--- WebApplication1/Creative_Harmony/wwwroot/js/site.js
+++ WebApplication1/Creative_Harmony/wwwroot/js/site.js
@@ -2,3 +2,17 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+$("#btnFullPro").click(function () {
+
+    $('html, body').animate({
+        scrollTop: $("#Divpro").offset().top -70
+    }, 2000);
+    //$("#backtotop").show();
+});
+
+$("#backtotop").click(function () {
+    $('html, body').animate({
+        scrollTop: $("#btnFullPro").offset().top - 70
+    }, 2000);
+    //$("#backtotop").hide();
+});
