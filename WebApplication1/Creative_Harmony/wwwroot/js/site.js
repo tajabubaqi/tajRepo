0fcabf6d62bc6bb3f29166cba319b0de6c5a6902
tajabubaqi89@gmail.com <tajabubaqi89@gmail.com>
2018-11-05 18:21:24 +0200

    preLoader

--- WebApplication1/Creative_Harmony/wwwroot/js/site.js
+++ WebApplication1/Creative_Harmony/wwwroot/js/site.js
@@ -2,6 +2,14 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+$(document).ready(function () {
+
+   
+$(window).on('load', function () {
+    $('.preloader').delay(4000).fadeOut('slow');
+    $('.center').delay(5000).fadeOut('slow');
+}); 
+
 $("#btnFullPro").click(function () {
 
     $('html, body').animate({
@@ -15,4 +23,6 @@ $("#backtotop").click(function () {
         scrollTop: $("#btnFullPro").offset().top - 70
     }, 2000);
     //$("#backtotop").hide();
+    });
+
 });
